468a12048e22d18b8c11663f8a125f57b0ce2e6a
kent <nwekep14@gmail.com>
2023-01-04 21:36:34 +0100

    - solved the find sum array algo

--- DSAs/ARRAYS/array-algo.js
+++ DSAs/ARRAYS/array-algo.js
@@ -1,8 +1,9 @@
 // FIND TWO ARRAY ELEMENTS IN AN ARRAY THAT ADD UP TO A NUMBER
 
+// Time complexity: o(n^2)
 const findSum = (arr, target) => {
-	for (var i = 0, arrLength = arr.length; i < arrLength; i++) {
-		for (var j = i + 1; j < arrLength; j++) {
+	for (let i = 0, arrLength = arr.length; i < arrLength; i++) {
+		for (let j = i + 1; j < arrLength; j++) {
 			if (arr[i] + arr[j] == target) {
 				return [i, j];
 			}
@@ -11,4 +12,25 @@ const findSum = (arr, target) => {
 	return -1;
 };
 
-console.log(findSum([1, 2, 3, 4, 5], 6));
+// console.log(findSum([1, 2, 3, 4, 5], 6));
+
+// Using HashTable
+
+const findSumBetter = (arr, weight) => {
+	var hashtable = {};
+
+	for (var i = 0; i < arr.length; i++) {
+		var currentElement = arr[i],
+			difference = weight - currentElement;
+		//		check the right one already exists
+		if (hashtable[currentElement] != undefined) {
+			return [i, hashtable[weight - currentElement]];
+		} else {
+			// store index
+			hashtable[difference] = i;
+		}
+	}
+	return -1;
+};
+
+console.log(findSumBetter([1, 2, 3, 4, 5], 9));
