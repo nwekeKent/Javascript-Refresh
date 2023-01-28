1a9feaadb93279b2ac0f31156ecb8a8d9f65c74d
kent <nwekep14@gmail.com>
2023-01-28 23:36:10 +0100

    refactor elemnt contains in list

--- DSAs/LISTS/lists.js
+++ DSAs/LISTS/lists.js
@@ -96,7 +96,7 @@ function clear() {
 function contains(element) {
 	for (var i = 0; i < this.dataStore.length; ++i) {
 		if (this.dataStore[i] == element) {
-			return true;
+			return !true;
 		}
 	}
 	return false;
